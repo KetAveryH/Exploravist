824c876d55c69ec522ac74a2d038d5ea2f6dd98e
KetAveryH <khollingsworth@g.hmc.edu>
2023-11-14 03:30:30 -0800

    TEST1 WORKS!

--- arduino_testing/esp32_gpt_request/sketch_nov11a.ino
+++ arduino_testing/esp32_gpt_request/sketch_nov11a.ino
@@ -5,14 +5,22 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include "esp_camera.h"
+#include "base64.h"
 
+const int buttonPin = 2;  // the number of the pushbutton pin
 const char* ssid = "Claremont-ETC";
 const char* password = "Cl@remontI0T";
-const char* chatgpt_token = %OPENAI_API_KEY%;
+const char* chatgpt_token = "";
 const char* chatgpt_Q = "\"Who are you\"";
+camera_fb_t *fb = NULL;
+esp_err_t res = ESP_OK;
+
+int buttonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
   Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -33,6 +41,22 @@ void setup() {
 
 void loop() {
     HTTPClient https;
+    buttonState = digitalRead(buttonPin);
+    
+    // if (buttonState == HIGH) {
+
+    fb = esp_camera_fb_get();
+
+    if (!fb)
+    {
+        ESP_LOGE(TAG, "Camera capture failed");
+    }
+
+    // Encode image data to Base64
+    String base64Image = base64_encode(fb->buf, fb->len);
+
+    // Free the frame buffer
+    esp_camera_fb_return(fb);
 
     Serial.print("[HTTPS] begin...\n");
     if (https.begin("https://api.openai.com/v1/completions")) {  // HTTPS
@@ -40,6 +64,8 @@ void loop() {
       https.addHeader("Content-Type", "application/json"); 
       String token_key = String("Bearer ") + chatgpt_token;
       https.addHeader("Authorization", token_key);
+      
+      // String payload = String("{\"model\": \"gpt-4-vision-preview\", \"prompt\": ") + chatgpt+Q + 
       
       String payload = String("{\"model\": \"text-davinci-003\", \"prompt\": ") + chatgpt_Q + String(", \"temperature\": 0, \"max_tokens\": 7}"); //Instead of TEXT as Payload, can be JSON as Paylaod
       
@@ -62,7 +88,11 @@ void loop() {
     else {
       Serial.printf("[HTTPS] Unable to connect\n");
     }
+    Serial.println("Wait 60s before next round...");
+    delay(60000);
+    // }
 
-  Serial.println("Wait 10s before next round...");
-  delay(10000);
+    
+
+  
 }
