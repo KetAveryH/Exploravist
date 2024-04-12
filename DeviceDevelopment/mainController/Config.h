a83844d2c85c8f705ba8665f7254fd5cb36ff58c
KetAveryH <khollingsworth@g.hmc.edu>
2024-04-11 23:13:40 -0700

    Attempting to fix merge deletions

---
+++ DeviceDevelopment/mainController/Config.h
@@ -0,0 +1,4 @@
+const char *ssid = "Claremont-ETC";
+const char *password = "Cl@remontI0T";
+const char *gpt_token = "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";
+const char *anthropic_key = "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";
