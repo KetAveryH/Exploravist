acf2cf4d1654ce60148cafd3803881e3a9ea0c5a
KetAveryH <khollingsworth@g.hmc.edu>
2024-04-15 22:25:09 -0700

    reintroduced example code

---
+++ DeviceDevelopment/example_code/0TA_Template_Sketch/0TA_Template_Sketch.ino
@@ -0,0 +1,23 @@
+#define ESP32_RTOS  // Uncomment this line if you want to use the code with freertos only on the ESP32
+                    // Has to be done before including "OTA.h"
+
+#include "OTA.h"
+#include <credentials.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+
+  setupOTA("OG ESP32", mySSID, myPASSWORD);
+
+  // Your setup code
+}
+
+void loop() {
+#ifdef defined(ESP32_RTOS) && defined(ESP32)
+#else // If you do not use FreeRTOS, you have to regulary call the handle method.
+  ArduinoOTA.handle();
+#endif
+
+  // Your code here
+}
---
+++ DeviceDevelopment/example_code/I2SaudioFromSD_MMC/I2SaudioFromSD_MMC.ino
@@ -0,0 +1,109 @@
+//**********************************************************************************************************
+//*    audioI2S-- I2S audiodecoder for ESP32,                                                              *
+//**********************************************************************************************************
+//
+// first release on 11/2018
+// Version 3  , Jul.02/2020
+//
+// THE SOFTWARE IS PROVIDED "AS IS" FOR PRIVATE USE ONLY, IT IS NOT FOR COMMERCIAL USE IN WHOLE OR PART OR CONCEPT.
+// FOR PERSONAL USE IT IS SUPPLIED WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHOR
+// OR COPYRIGHT HOLDER BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+//
+
+#include "Arduino.h"
+#include "WiFiMulti.h"
+#include "AudioESP.h"
+#include "SPI.h"
+#include "SD.h"
+#include "SD_MMC.h"
+#include "FS.h"
+
+// Digital I/O used
+#define SD_CS          5
+#define SPI_MOSI      23
+#define SPI_MISO      19
+#define SPI_SCK       18
+
+#define SD_MMC_CMD 38 //Please do not modify it.
+#define SD_MMC_CLK 39 //Please do not modify it. 
+#define SD_MMC_D0  40 //Please do not modify it.
+
+// #define I2S_DOUT      25
+// #define I2S_BCLK      27
+// #define I2S_LRC       26
+
+#define I2S_LRC       45 // LR Clock (LRC), was 26
+#define I2S_BCLK      47 // Bit Clock (BCLK), was 27
+#define I2S_DOUT      21 // Data Out (DIN), was 25
+
+
+
+AudioESP audio;
+
+
+void setup() {
+    // pinMode(SD_CS, OUTPUT);      digitalWrite(SD_CS, HIGH);
+    // SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    // SPI.setFrequency(1000000);
+    Serial.begin(115200);
+    // SD.begin(SD_CS);
+
+    SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+    if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
+      Serial.println("Card Mount Failed");
+      return;
+    }
+    uint8_t cardType = SD_MMC.cardType();
+    if(cardType == CARD_NONE){
+        Serial.println("No SD_MMC card attached");
+        return;
+    }
+
+
+    audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+    audio.setVolume(12); // 0...21
+
+   audio.connecttoFS(SD_MMC, "Battery 10 .wav");
+}
+
+void loop()
+{
+    audio.loop();
+    if(Serial.available()){ // put streamURL in serial monitor
+        audio.stopSong();
+        String r=Serial.readString(); r.trim();
+        if(r.length()>5) audio.connecttohost(r.c_str());
+        log_i("free heap=%i", ESP.getFreeHeap());
+    }
+}
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
