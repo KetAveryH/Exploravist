12ffb25232aff9e30a4c2fc0ff0cd7af0f7d9589
bilal-abraham <bilal.abraham.717@gmail.com>
2024-05-23 15:59:14 -0700

    react/vite setup

---
+++ WebDevelopment/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Fragment } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <Fragment>
+      App
+    </Fragment>
+  )
+}
+
+export default App
+
