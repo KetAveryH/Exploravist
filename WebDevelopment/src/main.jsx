ccd5309c27efd7d119bcf765b20e17696b2eb38c
bilal-abraham <bilal.abraham.717@gmail.com>
2024-06-04 16:57:07 -0700

    routing editing

--- WebDevelopment/src/main.jsx
+++ WebDevelopment/src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import Home from './routes/Home.jsx'
 import Updates from './routes/Updates.jsx'
 import Contact from './routes/Contact.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
-  {path: '/', element: <App />},
+  {path: '/', element: <Home />},
   {path: '/updates', element: <Updates />},
   {path: '/contact', element: <Contact />}
 ])
---
+++ WebDevelopment/src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Fragment } from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <Fragment>
+        <Navbar />
+        <p>Home</p>
+    </Fragment>
+  )
+}
+
+export default Home
