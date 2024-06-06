316e3e60fe53a390f772c5b0d52286ae9b8802e6
bilal-abraham <bilal.abraham.717@gmail.com>
2024-06-06 00:44:52 -0700

    first home design

--- WebDevelopment/src/routes/Home.jsx
+++ WebDevelopment/src/routes/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Fragment } from 'react'
 import Navbar from '../components/Navbar'
+import HomeLogo from '../assets/home-logo.svg'
+import '../styles/Home.css';
 
 const Home = () => {
   return (
     <Fragment>
         <Navbar />
-        <p>Home</p>
+        <section className='home_hero'>
+          <img className='home_logo' src={HomeLogo} alt='Home Logo' />
+        </section>
+        
     </Fragment>
   )
 }
