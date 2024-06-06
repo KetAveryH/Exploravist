eb724afa7cea9d3b6f8126cf78677dc911078f37
bilal-abraham <bilal.abraham.717@gmail.com>
2024-06-06 00:19:42 -0700

    first navbar design

--- WebDevelopment/src/components/Navbar.jsx
+++ WebDevelopment/src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { Link } from "react-router-dom";
+import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
     <Fragment>
-        <Link to='/'>Home</Link>
-        <Link to='/updates'>Updates</Link>
-        <Link to='/contact'>Contact</Link>
+      <section className='navbar_gradient'>
+        <div className='navbar_gradient_bar_1'/>
+        <div className='navbar_gradient_bar_2'/>
+        <div className='navbar_gradient_bar_3'/>
+      </section>
+      <nav className='navbar'>
+        <Link to='/' className='navbar_title'>ExploraVist</Link>
+				<ul className='navbar_nav'> 
+          <Link to='/updates' className='nav_link'>Updates</Link>
+          <Link to='/contact' className='nav_link'>Contact</Link>
+        </ul>
+			</nav>
     </Fragment>
   )
 }
