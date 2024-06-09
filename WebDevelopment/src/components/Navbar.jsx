55f8be21b725898d72687e3c009546857ad76a9b
bilal-abraham <bilal.abraham.717@gmail.com>
2024-06-08 22:01:52 -0700

    updated and polished navbar --subject to change

--- WebDevelopment/src/components/Navbar.jsx
+++ WebDevelopment/src/components/Navbar.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { Link } from "react-router-dom";
+import NavLogo from '../assets/nav-logo.svg'
 import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
     <Fragment>
-      {/* <section className='navbar_gradient'>
-        // <div className='navbar_gradient_bar_1'/>
-        // <div className='navbar_gradient_bar_2'/>
-        // <div className='navbar_gradient_bar_3'/>
-      </section>  */}
       <nav className='navbar'>
-        <Link to='/' className='navbar_title'>ExploraVist</Link>
-				<ul className='navbar_nav'> 
-          <Link to='/updates' className='nav_link'>UPDATES</Link>
-          <Link to='/contact' className='nav_link'>CONTACT</Link>
-        </ul>
+        <Link to='/' className='navbar_title'><img className='nav_logo' src={NavLogo} alt='ExploraVist' /></Link>
+        <Link to='/updates' className='nav_link'>Updates</Link>
+        <Link to='/contact' className='nav_link'>Contact</Link>
 			</nav>
     </Fragment>
   )
