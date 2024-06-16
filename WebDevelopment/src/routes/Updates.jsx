617ead5696d8c50e8d48ecbb2a1954b64f230683
bilal-abraham <bilal.abraham.717@gmail.com>
2024-06-15 19:30:19 -0700

    toggle details created with arrow animation

--- WebDevelopment/src/routes/Updates.jsx
+++ WebDevelopment/src/routes/Updates.jsx
@@ -8,9 +8,9 @@ import '../styles/Updates.css'
 
 
 const Updates = () => {
-  const [genOneDetails, setGenOneDetails] = useState(false);
-  const [genTwoDetails, setGenTwoDetails] = useState(false);
-  const [genThreeDetails, setGenThreeDetails] = useState(false);
+  const [genOneDetails, setGenOneDetails] = useState(true);
+  const [genTwoDetails, setGenTwoDetails] = useState(true);
+  const [genThreeDetails, setGenThreeDetails] = useState(true);
 
   return (
     <Fragment>
@@ -20,28 +20,14 @@ const Updates = () => {
             <h1 className='updates_header'>Progress Updates</h1>
             <div className='updates_gen'>
               <GenerationTitle gen_num='3' gen_date='04.21.24' gen_title='Our ready for market prototype!'/>
-              {/* fix details box sizing && everything else + details styling --might have to remove details tag and animate between chevrons && shrink margin between titles*/}
-              <details className='updates_details' onClick={()=>setGenThreeDetails(!genThreeDetails)}/>
+              <span className='updates_details_toggle' onClick={() => setGenThreeDetails(!genThreeDetails)}>
+                <ChevronDown className={`icon ${genThreeDetails ? 'rotate-down' : 'rotate-right'}`} size={30}/>
+                <strong>Details</strong>
+              </span>
               {genThreeDetails && 
-                  <div>Gen 3 Details</div>
+                <div className='updates_details'>Gen 3 Details</div>
               }
             </div>
-            <div className='updates_gen'>
-              <GenerationTitle gen_num='2' gen_date='02.15.24' gen_title='Device works (Still slowly) with battery safety and capacitive touch UI'/>
-              <details className='updates_details' onClick={()=>setGenTwoDetails(!genTwoDetails)}/>
-              {genTwoDetails && 
-                <div>Gen 2 Details</div>
-              }
-            </div>
-            <div className='updates_gen'>
-              <GenerationTitle gen_num='1' gen_date='12.25.23' gen_title='First working version'/>
-              <details className='updates_details' onClick={()=>setGenOneDetails(!genOneDetails)}/>
-              {genOneDetails && 
-                <div>Gen 1 Details</div>
-              }
-            </div>
-            <ChevronRight />
-            <ChevronDown />
           </section>
         </main>
         <Footer/>
